1f7c5b41eb50f26b4def422fbaf3df14d62c3386
alifzwan <alifzakwan529@gmail.com>
2024-05-29 16:09:25 +0800

    chore: Update Main component to include AppBar and improve styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-import RepositoryList from './RepositoryList'
+import { StyleSheet, View } from 'react-native';
+import RepositoryList from './Item/RepositoryList'
+
+import theme from '../theme';
+import AppBar from './Bar/AppBar';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.backgroundColor.primary,
+    
+  }
+})
 
 const Main = () => {
   return (
-    <RepositoryList />
+    <View style={styles.container}>
+      <AppBar />
+      <RepositoryList />
+    </View>
+    
   )
 }
 
