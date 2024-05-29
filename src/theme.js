9b23e21bc7de614c465dfc6dfa61f4a778a2e6bb
alifzwan <alifzakwan529@gmail.com>
2024-05-29 16:10:18 +0800

    refactor: Update theme.js to include primary and secondary background colors

---
+++ src/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+    colors: {
+        textPrimary: '#e1e4e8', //white
+        textSecondary: '#586069', //grey
+    },
+    backgroundColor:{
+        primary  : '#e1e4e8',   // white 
+        secondary: '#0366d6', // blue
+        tertiery : '#24292e',  //grey
+    },
+    fontWeight: {
+        normal: '400',
+        bold: '700',
+    },
+    fontSize: {
+        body: 14,
+        subheading: 15,
+    }
+}
+module.exports = theme;
