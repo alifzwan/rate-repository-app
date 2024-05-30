2c82cfeb85b4b9f0191b50730f4e64246503c98a
alifzwan <alifzakwan529@gmail.com>
2024-05-30 15:21:44 +0800

    refactor: Add NativeRouter and StatusBar to App component

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import Main from './src/components/Main';
+import { StatusBar } from 'expo-status-bar'
+import { NativeRouter } from 'react-router-native';
 
 export default function App() {
   return (
-    <Main />
+    <>
+      <NativeRouter>
+        <Main />
+      </NativeRouter>
+      <StatusBar style="auto" />
+    </>
   );
 }
 
