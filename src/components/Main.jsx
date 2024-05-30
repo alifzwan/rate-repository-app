601e42259745d05ea377699b0f088c8984ec7cc9
alifzwan <alifzakwan529@gmail.com>
2024-05-30 15:22:42 +0800

    refactor: Update Main component to use react-router-native for navigation

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,15 @@
 import { StyleSheet, View } from 'react-native';
 import RepositoryList from './Item/RepositoryList'
-
 import theme from '../theme';
 import AppBar from './Bar/AppBar';
+import { Routes, Route, Navigate } from 'react-router-native';
+import SignIn from './Item/SignIn';
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.backgroundColor.primary,
+    flexGrow: 1,
+    flexShrink: 1,
     
   }
 })
@@ -15,7 +18,11 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />}/>
+        <Route path="*" element={<Navigate to="/" replace/>}/>
+        <Route path="/signin" element={<SignIn />}/>
+      </Routes>
     </View>
     
   )
