2e68ab4e9fc56f489f5ee0892c9b147645033243
alifzwan <alifzakwan529@gmail.com>
2024-05-31 14:07:35 +0800

    refactor: Update App.js to use ApolloProvider for GraphQL integration

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import Main from './src/components/Main';
 import { StatusBar } from 'expo-status-bar'
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client';
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 export default function App() {
   return (
     <>
-      <NativeRouter>
-        <Main />
+    
+      <StatusBar style="auto" /> 
+      <NativeRouter>   {/* NativeRouter wraps the entire application and provides the routing capabilities. */}
+        {/* ApolloProvider wraps the Main component and provides the Apollo Client instance to the entire application. */}
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
-      <StatusBar style="auto" />
     </>
   );
 }
